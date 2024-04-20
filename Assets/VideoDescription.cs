48edc0418f8157a57cfbedd9cb51d9d18289fe54
mkae21 <bbuchu123@gmail.com>
2024-04-20 23:58:21 +0900

    Camera 구현
    Camera 구현 완료

--- Assets/VideoDescription.cs
+++ Assets/VideoDescription.cs
@@ -8,4 +8,5 @@
    transform의 이동은 컴퓨터의 성능 차이에 의해 달라지면 안되기 때문에 Time.deltaTime을 꼭 사용 해 주어야 한다.
    transform 이동은 물리 충돌을 무시하는 경우가 발생함 -> rigidbody의 Collision Detection을 Continuos로 변경
 
+   현재 이 예시는 기본이 뛰는 것, shift를 누르면 걷기
  */
